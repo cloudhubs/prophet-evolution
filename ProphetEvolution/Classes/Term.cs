78930e8c03cd15b400e402a53a2c0c92f48b7829
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2024-02-17 14:30:36 -0700

    Create self-hierarchy

--- ProphetEvolution/Classes/Term.cs
+++ ProphetEvolution/Classes/Term.cs
@@ -3,6 +3,13 @@
     public class Term
     {
         public string Name {  get; set; }
-        public string Definition { get; set; }
+
+        public string Definition { get; set; } = null;
+
+        public List<string>? Alias { get; set; }
+
+        public string? Parent { get; set; } = null;
+
+
     }
 }
