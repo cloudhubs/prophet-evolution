4c66e72adf271d9763a66d6aae9432493f3cfee3
ricardo-boone <ricohboone@gmail.com>
2024-05-07 12:13:12 -0500

    added IncrementalCimetRunner

--- src/test/java/edu/university/ecs/lab/full/FullIRConstructionTest.java
+++ src/test/java/edu/university/ecs/lab/full/FullIRConstructionTest.java
@@ -1,37 +1,29 @@
 package edu.university.ecs.lab.full;
 
-import edu.university.ecs.lab.common.config.ConfigUtil;
-import edu.university.ecs.lab.common.config.models.InputConfig;
-import edu.university.ecs.lab.common.utils.FullCimetUtils;
-import edu.university.ecs.lab.delta.DeltaExtraction;
-import edu.university.ecs.lab.intermediate.create.IRExtraction;
+import edu.university.ecs.lab.IncrementalCimetRunner;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.Collections;
 
-import edu.university.ecs.lab.intermediate.merge.IRMergeRunner;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.RepositoryBuilder;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
-import org.junit.jupiter.api.BeforeAll;
+import edu.university.ecs.lab.common.utils.FullCimetUtils;
+import edu.university.ecs.lab.intermediate.create.IRExtraction;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FullIRConstructionTest {
 
   private final static String inputConfigPath = "config-full-test.json";
   private final static String baseBranch = "main";
   private final static String baseCommit = "7874340529b1f7094c8c6682e0dc1c37c8033ac2";
-//  private final static String baseCommit = "7abcaf2ea0cdd04d68bb7909799bc37302b254b8";
+  private final static String endCommit = "13f93fae687efebe8b931b58b953fc4a2c2df222";
 
   private static final Logger LOGGER = Logger.getLogger(FullIRConstructionTest.class.getName());
 
@@ -49,90 +41,38 @@ class FullIRConstructionTest {
     }
   }
 
-  @BeforeAll
-  public static void setUp() {
-    // Run IR Extraction on the base commit
-    String[] IRArgs = {inputConfigPath, baseBranch, baseCommit};
-    try {
-      IRExtraction.main(IRArgs);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-
-  }
 
   @Test
-  public void fullIRConstructionTest() throws Exception {
+  public void fullIRComparisonTest(){
+    ObjectMapper mapper = new ObjectMapper();
 
-    InputConfig inputConfig = ConfigUtil.validateConfig(inputConfigPath);
-    String repoPath = inputConfig.getClonePath() + File.separator + "train-ticket-microservices";
+    JsonNode json1 = null, json2 = null;
 
-    Repository repo = new RepositoryBuilder().setGitDir(new File(repoPath, ".git")).build();
-
-    try (Git git = new Git(repo)) {
-
-      // Fetch updates from the remote repository
-      git.fetch().setRemote("origin").call();
-
-      // Get all commits from the remote branch
-      Iterable<RevCommit> commitsIterable = git.log().add(repo.resolve("refs/remotes/origin/" + baseBranch)).call();
-
-      // Convert Iterable to List
-      List<RevCommit> commitsList = StreamSupport.stream(commitsIterable.spliterator(), false).collect(Collectors.toList());
-
-      // Reverse the list
-      Collections.reverse(commitsList);
-
-      int count = 0;
-      boolean isBaseCommit = false;
-      boolean isMerged = false;
-
-      for (RevCommit commit : commitsList) {
-
-        if (!isBaseCommit && commit.getName().equals(baseCommit)) {
-          LOGGER.info("Beginning IR Extraction: " + baseCommit);
-
-          // Run IR Extraction on the base commit
-          String[] IRArgs = {inputConfigPath, baseBranch, baseCommit};
-          IRExtraction.main(IRArgs);
-
-          isBaseCommit = true;
-        }
-        else if (isBaseCommit) {
-          LOGGER.info("Beginning Delta Extraction: " + commit.getName());
-
-
-          // Run Delta Extraction on next commit
-          String[] deltaArgs = {baseBranch, commit.getName(), inputConfigPath};
-          DeltaExtraction.main(deltaArgs);
-
-
-          if (isBaseCommit && !isMerged) {
-            LOGGER.info("Beginning Merge IR Extraction: " + commit.getName());
-
-            // Run IR Merge Runner on IR and Merge
-            String[] mergeArgs = {FullCimetUtils.pathToIR, FullCimetUtils.pathToDelta, inputConfigPath, baseBranch, commit.getName()};
-            IRMergeRunner.main(mergeArgs);
-
-            isMerged = true;
-          }
-          else {
-            LOGGER.info("Beginning Merge IR Extraction: " + commit.getName());
-
-            String[] mergeArgs = {FullCimetUtils.pathToNewIR, FullCimetUtils.pathToDelta, inputConfigPath, baseBranch, commit.getName()};
-            IRMergeRunner.main(mergeArgs);
-          }
-
-        }
-
-
-
-        count++;
-      }
-
-      System.out.println(FullCimetUtils.pathToNewIR);
-
-      System.out.println("Total commits: " + count);
+    // Run IncrementalCimetRunner
+    try {
+      IncrementalCimetRunner.main(new String[]{inputConfigPath, baseBranch, baseCommit, endCommit});
+      json1 = mapper.readTree(new File(FullCimetUtils.pathToNewIR));
     }
+    catch (Exception e) {
+      LOGGER.severe("Error in IncrementalCimetRunner: " + e.getMessage());
+      assert false;
+    }
+
+
+    // Run IR Extraction
+    try {
+      IRExtraction.main(new String[]{inputConfigPath, baseBranch, endCommit});
+      json2 = mapper.readTree(new File(FullCimetUtils.pathToIR));
+    }
+    catch (Exception e) {
+      LOGGER.severe("Error in IR Extraction: " + e.getMessage());
+      assert false;
+    }
+
+    // Compare the JSON objects
+    boolean isEqual = json1.equals(json2);
+
+    assertTrue(isEqual);
+
   }
 }
